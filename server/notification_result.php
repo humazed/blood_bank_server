c87252a8654d6b048934fa84fb4b06372d25912e
humazed <humazed@gmail.com>
2017-06-17 01:07:58 +0200

    fix id in notification_result.php

--- server/notification_result.php
+++ server/notification_result.php
@@ -56,6 +56,8 @@ if (!isset($obj->{'response'})) {
 //    }
 //
 //    echo '</table>';
+
+
 //    echo $response;
 }
 
@@ -69,38 +71,36 @@ $query = "SELECT id,response FROM response";
 // and the query
 $rows = $dbo->query($query);
 
-//if ($rows->rowCount() > 0) {
-//    echo '<table align="left"
-//cellspacing="5" cellpadding="8">
-//
-//<tr><td align="left"><b>id</b></td>
-//<td align="left"><b>response</b></td></tr>';
-//
-//// mysqli_fetch_array will return a row of data from the query
-//// until no further data is available
-//    foreach ($rows as $row) {
-//        echo '<tr><td align="left">' .
-//            $row['id'] . '</td><td align="left">' .
-//            $row['response'] . '</td>';
-//
-//        echo '</tr>';
-//    }
-//
-//    echo '</table>';
-//
-//} else {
-//    echo "Couldn't issue database query<br />";
-//}
-
 // If the query executed properly proceed
 if ($rows->rowCount() > 0) {
+//    echo "\n\nID \t response\n\n";
+//    echo "\n\n123456 \t accepted\n\n";
+
 
 // mysqli_fetch_array will return a row of data from the query
 // until no further data is available
-    echo "id\tresponse";
     foreach ($rows as $row) {
-        echo $rows['id'] . "   \t   " . $row['response'];
+        echo "   \t" . $row['response'];
         echo "\n\n";
     }
 
+}
+if ($rows->rowCount() > 0) {
+    echo '<table align="left"
+cellspacing="5" cellpadding="8">
+
+<tr><td align="left"><b>ID</b></td>
+<td align="left"><b>response </b></td></tr>';
+
+// mysqli_fetch_array will return a row of data from the query
+// until no further data is available
+    foreach ($rows as $row) {
+        echo '<tr><td align="left">' .
+            $row['id'] . '</td><td align="left">' .
+            $row['response'] . '</td>';
+
+        echo '</tr>';
+    }
+
+    echo '</table>';
 }
